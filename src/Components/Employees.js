599697866f8f74d2fd9b98a8ec588961de17f61e
Varalakshmi <varalakshmi@srsofttech.in>
2022-09-09 12:33:48 +0530

    crud finished

---
+++ src/Components/Employees.js
@@ -0,0 +1,18 @@
+const Employees = [
+    {
+        id:"1",
+        Name:"Harry potter",
+        Age:"20"
+    },
+    {
+        id:"2",
+        Name:"Hermoine granger",
+        Age:"19",
+    },
+    {
+        id:"3",
+        Name:"Ron",
+        Age:"21",
+    }
+]
+export default Employees
